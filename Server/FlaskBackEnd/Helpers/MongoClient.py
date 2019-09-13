b6beab542a324ece3313692362da5f80b2d45823
Hewage, Asiri <asiri.hewage@pearson.com>
2019-09-13 22:03:32 +0530

    Fllask  UI

--- Server/FlaskBackEnd/Helpers/MongoClient.py
+++ Server/FlaskBackEnd/Helpers/MongoClient.py
@@ -14,17 +14,17 @@ class MongoClient:
         self.logManager = LogManager()
         self.enableDebug = self.config.getConfig('system', 'debug')
         try:
-            print('Connecting to MongoDB Altas....')
+            print('[DB Connection] \t Connecting to MongoDB Altas....')
             client = pymongo.MongoClient(self.config.getConfig('app', 'dbMongo'))
             self.db = client.test
             if self.enableDebug:
-                self.logManager.log('[DB Connection] Connecting to MongoDB Altas....')
-            print('MongoDB Atlas Connected.')
+                self.logManager.log('[DB Connection] \t Connecting to MongoDB Altas....')
+            print('[DB Connection] \t MongoDB Atlas Connected.')
 
         except Exception as e:
             if self.enableDebug:
                 self.logManager.log(str(e))
-            print('[DB Connection - Exception] ', e)
+            print('[DB Connection] \t ', e)
             quit()
 
     def connect(self):
