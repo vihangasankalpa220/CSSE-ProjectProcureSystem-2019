6f69fd5f68d86bbccdf02b228d35cd9d8e6694d1
iamdempa <jbjayarathna@gmail.com>
2019-09-14 00:55:24 +0530

    added dashboard

--- Client/src/components/banuka/Banuka.jsx
+++ Client/src/components/banuka/Banuka.jsx
@@ -1,11 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import TopNavigation from "./topNavigation";
+import SideNavigation from "./sideNavigation";
+import Footer from "./Footer";
+import './index.css';
 
-export default class Banuka extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Hello Banuka</h1>
-            </div>
-        );
-    }
+export default class Banuka extends Component {
+  render() {
+    return (
+      <div className="flexible-content">
+        {/* <TopNavigation /> */}
+        <SideNavigation />
+        <main id="content" className="p-5">
+          
+        </main>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
 }
