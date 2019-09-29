7b88fd3a5ae3d2d671c5f66cce6e09924d687588
VihangaSankalpa220 <vihangasankalpa220@gmail.com>
2019-09-29 00:01:10 -0700

    all

---
+++ Server/Express/routes/business.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Business
+let Business = new Schema({
+  order_no :{
+    type:String
+  },
+  po_number:{
+    type:String
+  },
+  currency:{
+    type:String
+  },
+  subtotal:{
+    type:Number
+  },
+  person_name: {
+    type: String
+  },
+  business_name: {
+    type: String
+  },
+  qty: {
+    type: Number
+  },
+  payment:{
+    type:String
+  },
+  bill_address:{
+    type:String
+  },
+  ship_address:{
+    type:String
+  },
+  slot:{
+    type:String
+  },
+  status:{
+    type:String
+  }
+},{
+    collection: 'business'
+});
+
+module.exports = mongoose.model('Business', Business);
